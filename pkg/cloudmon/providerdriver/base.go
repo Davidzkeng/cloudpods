ee5ee4f712033f3b527306b89542bd610e6c503f
huangzekeng <huangzekeng@grgbanking.com>
2022-10-25 10:57:25 +0800

    修复品高获取不到监控数据问题

--- pkg/cloudmon/providerdriver/base.go
+++ pkg/cloudmon/providerdriver/base.go
@@ -17,6 +17,7 @@ package providerdriver
 import (
 	"context"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -293,27 +294,34 @@ func (self *SCollectByResourceIdDriver) CollectHostMetrics(ctx context.Context, 
 	var mu sync.Mutex
 	for i := range res {
 		wg.Add(1)
-		go func(vm api.HostDetails) {
+		go func(host api.HostDetails) {
 			defer func() {
 				wg.Done()
 			}()
+			resourceId := host.ExternalId
+			if strings.Contains(host.ExternalId, "@") {
+				info := strings.Split(host.ExternalId, "@")
+				if len(info) == 2 {
+					resourceId = info[1]
+				}
+			}
 			opts := &cloudprovider.MetricListOptions{
 				ResourceType: cloudprovider.METRIC_RESOURCE_TYPE_HOST,
-				ResourceId:   vm.ExternalId,
-				RegionExtId:  vm.RegionExtId,
+				ResourceId:   resourceId,
+				RegionExtId:  host.RegionExtId,
 				StartTime:    start,
 				EndTime:      end,
 			}
 
 			tags := []influxdb.SKeyValue{}
-			for k, v := range vm.GetMetricTags() {
+			for k, v := range host.GetMetricTags() {
 				tags = append(tags, influxdb.SKeyValue{
 					Key:   k,
 					Value: v,
 				})
 			}
 			pairs := []influxdb.SKeyValue{}
-			for k, v := range vm.GetMetricPairs() {
+			for k, v := range host.GetMetricPairs() {
 				pairs = append(pairs, influxdb.SKeyValue{
 					Key:   k,
 					Value: v,
@@ -322,7 +330,7 @@ func (self *SCollectByResourceIdDriver) CollectHostMetrics(ctx context.Context, 
 			data, err := provider.GetMetrics(opts)
 			if err != nil {
 				if errors.Cause(err) != cloudprovider.ErrNotImplemented && errors.Cause(err) != cloudprovider.ErrNotSupported {
-					log.Errorf("get host %s(%s) error: %v", vm.Name, vm.Id, err)
+					log.Errorf("get host %s(%s) error: %v", host.Name, host.Id, err)
 					return
 				}
 				return
